1b83daf7440778774d981dd236991b2cf63745c3
JC Ivancevich <jc.ivancevich@gmail.com>
2014-10-26 20:56:37 -0300

    start playing first result

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ var player = require('./lib/player');
 var kickass = require('./lib/kickass');
 var streamer = require('./lib/streamer');
 
-function getRandom(array) {
-    var i = Math.floor(Math.random() * (array.length));
-    return array[i];
-}
-
 kickass.search({
     query: process.argv[2],
     category: 'music'
 }, function(err, results) {
     if (err) return console.error(err);
-    streamer.start(getRandom(results), function(err, files) {
+    streamer.start(results[0], function(err, files) {
         if (err) return console.error(err);
         player.play(files);
     });
