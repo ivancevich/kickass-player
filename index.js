93b789dea6c2967e455d206f557be07bfe77bfb6
JC Ivancevich <jc.ivancevich@gmail.com>
2014-10-27 10:06:12 -0300

    randomize if no search was provided

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ var player = require('./lib/player');
 var kickass = require('./lib/kickass');
 var streamer = require('./lib/streamer');
 
+var query = process.argv[2];
+
+function getOne(array) {
+    if (query) {
+        return array[0];
+    }
+    var i = Math.floor(Math.random() * (array.length));
+    return array[i];
+}
+
 kickass.search({
-    query: process.argv[2],
+    query: query,
     category: 'music'
 }, function(err, results) {
     if (err) return console.error(err);
-    streamer.start(results[0], function(err, files) {
+    streamer.start(getOne(results), function(err, files) {
         if (err) return console.error(err);
         player.play(files);
     });
